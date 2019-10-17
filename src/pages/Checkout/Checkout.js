afbe44cea705b238e47281aa332e931c023f78c9
Gabriel Añez <artu242@gmail.com>
2019-10-16 21:49:49 -0400

    Conditional for stripe button

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -34,15 +34,21 @@ function Checkout({ cartItems, total }) {
       {cartItems.map(cartItem => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-      <div className="total">
-        <span>TOTAL: ${total}</span>
-      </div>
-      <div className="test-warning">
-        *Please use the following credit card for payment*
-        <br />
-        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
-      </div>
-      <StripeButton price={total} />
+      {total > 0 ? (
+        <>
+          <div className="total">
+            <span>TOTAL: ${total}</span>
+          </div>
+          <div className="test-warning">
+            *Please use the following credit card for payment*
+            <br />
+            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+          </div>
+          <StripeButton price={total} />
+        </>
+      ) : (
+        <span>Your cart is empty</span>
+      )}
     </div>
   );
 }
